7538aa7e9e7388d97e6ed21c6d622dd09f633e31
yasiru114 <yasirunadeesha0@gmail.com>
2026-07-19 10:07:24 +0530

    Refactor App class to implement Spring Boot application structure and add home endpoint

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,20 @@
 package com.mycompany.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
+
     public static void main(String[] args) {
-        String appName = System.getProperty("app.name");
-        String appVersion = System.getProperty("app.version");
-        System.out.println("App Name: " + appName);
-        System.out.println("App Version: " + appVersion);
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World from Spring Boot!";
     }
 }
